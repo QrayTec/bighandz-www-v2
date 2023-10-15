a35623cee03da78564d45f9ef8aa0e252aa5cbf3
shouzia <saopipizhu@163.com>
2023-10-15 14:28:32 +0800

    ✨完善主要顧客

--- src/components/IndexField.vue
+++ src/components/IndexField.vue
@@ -1,35 +1,89 @@
 <template>
   <div class="index_field">
     <div class="index_field_main">
-      <div class="index_field_Title">業種領域</div>
-      <div class="index_field_Nav">
-        <div
-          ref="NavLine"
-          class="Nav_Line"
-        ></div>
-        <div
-          v-for="(item, index) in NavData"
-          :key="index"
-          class="index_items items_uncheck"
-          :class="[
-            'index_items',
-            {
-              items_checked: selectedIndex === index,
-              items_uncheck: selectedIndex !== index
-            }
-          ]"
-          @click="selectNavItem(index)"
-        >
-          <div class="index_items_icon">
-            <img
-              :src="'src/assets/images/index_field/' + item.src + '.svg'"
-              alt=""
-            />
+      <div class="index_field_main_Nav">
+        <div class="index_field_Title">業種領域</div>
+        <div class="index_field_Nav">
+          <div
+            ref="NavLine"
+            class="Nav_Line"
+          ></div>
+          <div
+            v-for="(item, index) in NavData"
+            :key="index"
+            class="index_items items_uncheck"
+            :class="[
+              'index_items',
+              {
+                items_checked: selectedIndex === index,
+                items_uncheck: selectedIndex !== index
+              }
+            ]"
+            @click="selectNavItem(index)"
+          >
+            <div class="index_items_icon">
+              <img
+                :src="'src/assets/images/index_field/' + item.src + '.svg'"
+                alt=""
+              />
+            </div>
+            <div class="index_items_title">
+              <p class="index_items_title_cn">{{ item.title_cn }}</p>
+              <p class="index_items_title_en">{{ item.title_en }}</p>
+            </div>
           </div>
-          <div class="index_items_title">
-            <p class="index_items_title_cn">{{ item.title_cn }}</p>
-            <p class="index_items_title_en">{{ item.title_en }}</p>
+          <div class="index_field_flip">
+            <div
+              class="flip_btn arrow_icon_hover"
+              @click="shields(false)"
+            >
+              <img
+                class="arrow_icon_l"
+                src="../assets/images/index_field/arrow.svg"
+                alt=""
+              />
+              <div class="shields"></div>
+            </div>
+            <div
+              class="flip_btn arrow_icon_hover"
+              @click="shields(true)"
+            >
+              <img
+                src="../assets/images/index_field/arrow.svg"
+                alt=""
+              />
+              <div class="shields"></div>
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="index_field_content"
+      :class="'main_bg_' + selectedIndex"
+    >
+      <div
+        v-for="(item, index) in ContentData"
+        v-show="selectedIndex === index"
+        :key="index"
+        class="index_field_content_main animate__animated animate__fadeIn"
+      >
+        <div class="index_field_content_title">
+          <img
+            :src="'src/assets/images/index_field/' + item.src + '.svg'"
+            alt=""
+          />
+          <div class="content_title">{{ item.title_cn }}</div>
+        </div>
+        <div class="index_field_content_text">
+          <p>{{ item.title_1 }}</p>
+          <br /><br />
+          <p>
+            {{ item.title_2 }}
+          </p>
+          <p>
+            {{ item.title_3 }}
+          </p>
         </div>
       </div>
     </div>
@@ -37,7 +91,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
 interface NavDataType {
   id: number
@@ -45,6 +99,16 @@ interface NavDataType {
   title_cn: string
   title_en: string
 }
+
+interface COntentDataType {
+  id: number
+  src: string
+  title_cn: string
+  title_1: string
+  title_2: string
+  title_3: string
+}
+
 const selectedIndex = ref(0)
 const NavLine = ref<HTMLElement>()
 const NavData = ref<NavDataType[]>([
@@ -54,20 +118,75 @@ const NavData = ref<NavDataType[]>([
   { id: 3, src: 'icon_4', title_cn: '制造业', title_en: 'MANUFACTURE' }
 ])
 
+const ContentData = ref<COntentDataType[]>([
+  {
+    id: 0,
+    src: 'icon_1',
+    title_cn: '金融',
+    title_1: '主な業務経験は製造、流通関係です。',
+    title_2:
+      '製造業、物流サービス、通信のソフトウェア開発を得意分野としております。',
+    title_3:
+      'システムの最適な開発を行なえる様努力すると伴に、高品質、高性能ソフトウェアプロダクトを提供出来る事を目指しています。'
+  },
+  {
+    id: 1,
+    src: 'icon_2',
+    title_cn: '政府',
+    title_1: '主な業務経験は製造、流通関係です。',
+    title_2:
+      '製造業、物流サービス、通信のソフトウェア開発を得意分野としております。',
+    title_3:
+      'システムの最適な開発を行なえる様努力すると伴に、高品質、高性能ソフトウェアプロダクトを提供出来る事を目指しています。'
+  },
+  {
+    id: 2,
+    src: 'icon_3',
+    title_cn: '医疗',
+    title_1: '主な業務経験は製造、流通関係です。',
+    title_2:
+      '製造業、物流サービス、通信のソフトウェア開発を得意分野としております。',
+    title_3:
+      'システムの最適な開発を行なえる様努力すると伴に、高品質、高性能ソフトウェアプロダクトを提供出来る事を目指しています。'
+  },
+  {
+    id: 3,
+    src: 'icon_4',
+    title_cn: '制造业',
+    title_1: '主な業務経験は製造、流通関係です。',
+    title_2:
+      '製造業、物流サービス、通信のソフトウェア開発を得意分野としております。',
+    title_3:
+      'システムの最適な開発を行なえる様努力すると伴に、高品質、高性能ソフトウェアプロダクトを提供出来る事を目指しています。'
+  }
+])
+
 const selectNavItem = (index: number) => {
   NavLine.value!.style.left = `${250 * index}px`
   selectedIndex.value = index
+}
+
+const shields = (flag: boolean) => {
+  if (flag) {
+    selectedIndex.value = (selectedIndex.value + 1) % 4
+  } else {
+    selectedIndex.value = (selectedIndex.value - 1 + 4) % 4
+  }
+  selectNavItem(selectedIndex.value)
 }
 </script>
 
 <style scoped>
 .index_field {
   width: 100%;
-  display: flex;
-  justify-content: center;
   background-color: #111f4e;
 }
 .index_field_main {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.index_field_main_Nav {
   width: 1200px;
 }
 .index_field_Title {
@@ -92,6 +211,7 @@ const selectNavItem = (index: number) => {
   padding: 0 23px;
   display: flex;
   align-items: center;
+  cursor: pointer;
 }
 .index_items_title {
   color: #fff;
@@ -128,5 +248,92 @@ const selectNavItem = (index: number) => {
   left: 0;
   top: 0;
   transition: all 0.8s;
+}
+.index_field_flip {
+  margin-left: 72px;
+  padding: 20px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.flip_btn {
+  width: 64px;
+  height: 64px;
+  background-color: #0e1b47;
+  border: 1px solid #597be5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+}
+.arrow_icon_l {
+  transform: rotate(180deg);
+}
+.shields {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); /* 遮罩颜色和透明度 */
+  opacity: 0; /* 初始时不可见 */
+  transition: opacity 0.3s ease;
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.flip_btn:hover {
+  background-color: #0e1b47;
+  border: 1px solid #31437c;
+}
+.flip_btn:hover .shields {
+  opacity: 1;
+}
+.index_field_content {
+  min-height: 580px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+}
+.main_bg_0 {
+  background-image: url('../assets/images/index_field/bg1.png');
+}
+.main_bg_1 {
+  background-image: url('../assets/images/index_field/bg2.png');
+}
+.main_bg_2 {
+  background-image: url('../assets/images/index_field/bg3.png');
+}
+.main_bg_3 {
+  background-image: url('../assets/images/index_field/bg4.png');
+}
+
+.index_field_content_main {
+  width: 1200px;
+}
+.index_field_content_title {
+  margin-top: 90px;
+  padding: 0px 22px;
+  display: flex;
+  align-items: center;
+}
+.content_title {
+  color: #fff;
+  margin-left: 14px;
+  font-family: Noto Sans JP;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 54px; /* 150% */
+}
+.index_field_content_text {
+  margin-top: 45px;
+  padding: 0px 76px;
+  color: #fff;
+  font-family: Noto Sans JP;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px; /* 200% */
 }
 </style>
