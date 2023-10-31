1c27b524380189dafc303358a027d4ee1291ae35
QrayTec <saopipizhu@163.com>
2023-10-31 07:19:47 +0000

    Update from https://github.com/QrayTec/bighandz-www-v2/commit/bceb1e5de6ae4af8bdc5294ff1e5192c9ce3adc0

---
+++ assets/home-96aeaede.js
@@ -0,0 +1,1 @@
+import{g as c,r as n,o as s,c as r,b as e,h as a}from"./app-d112b10e.js";const _={},m={class:"home-page"};function i(p,f){const o=n("RouterView"),t=a;return s(),r("main",m,[e(o),e(t)])}const l=c(_,[["render",i]]);export{l as default};
