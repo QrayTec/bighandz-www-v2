e080ea2bb781b2b150b746b680362b051fe36a88
QrayTec <saopipizhu@163.com>
2023-11-22 05:46:19 +0000

    Update from https://github.com/QrayTec/bighandz-www-v2/commit/b733a352047f5cab917b51e8d09ed03367bc6f85

---
+++ assets/Company_staff_observation-ab21b6c6.js
@@ -0,0 +1,1 @@
+const a="/assets/Company_staff_observation-b7c9e73b.png";export{a as C};
---
+++ assets/adoptionCard-4cdf662a.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./adoptionCard.vue_vue_type_script_setup_true_lang-952ea2ce.js";import"./app-2539f404.js";export{m as default};
