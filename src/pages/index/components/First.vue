b8f60830a2a58fdd88553ee20302236e6d6f0792
shouzia <saopipizhu@163.com>
2023-11-23 22:44:34 +0800

    ✨新增自动

--- src/pages/index/components/First.vue
+++ src/pages/index/components/First.vue
@@ -25,39 +25,41 @@
           class="Scroll_Line mx-auto mt-[20px] h-[80px] w-[1px] flex-shrink-0 translate-y-[50%]"
         ></div>
       </div>
-      <div class="h-[77px] w-[1100px] flex translate-x-[-30px]">
+      <div class="absolute top-[120px]">
+        <div class="h-[77px] w-[1100px] flex translate-x-[-30px]">
+          <img
+            class="h-full"
+            src="/src/assets/images/index_main/speed_1.png"
+            alt=""
+          />
+          <div class="mx-[20px] h-full w-[5px] bg-white"></div>
+          <img
+            class="h-full"
+            src="/src/assets/images/index_main/speed_2.png"
+            alt=""
+          />
+        </div>
+        <div class="mt-[123.5px] w-[800px]">
+          <div
+            class="font-NotoSansJP h-[60px] w-[751px] flex items-center bg-[#597BE5] py-[3px] p-r-[12px] text-[36px] leading-none font-[700] not-italic text-white"
+          >
+            「IT×ビジネス×教育」× SPEED ＝ POWER
+          </div>
+          <div
+            class="font-NotoSansJP mt-[60px] text-[20px] font-[400] text-white"
+          >
+            <p>
+              ITからビジネスへ、ビジネスからITへ、教育からIT・ビジネスへと三位一体でスピーディに社会の発展に貢献しつづけます。
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="absolute bottom-[120px] right-[120px] h-[120px] w-[120px]">
         <img
-          class="h-full"
-          src="/src/assets/images/index_main/speed_1.png"
-          alt=""
-        />
-        <div class="mx-[20px] h-full w-[5px] bg-white"></div>
-        <img
-          class="h-full"
-          src="/src/assets/images/index_main/speed_2.png"
+          src="/src/assets/images/index_main/25th.png"
           alt=""
         />
       </div>
-      <div class="mt-[123.5px] w-[800px]">
-        <div
-          class="font-NotoSansJP h-[60px] w-[751px] flex items-center bg-[#597BE5] py-[3px] p-r-[12px] text-[36px] leading-none font-[700] not-italic text-white"
-        >
-          「IT×ビジネス×教育」× SPEED ＝ POWER
-        </div>
-        <div
-          class="font-NotoSansJP mt-[60px] text-[20px] font-[400] text-white"
-        >
-          <p>
-            ITからビジネスへ、ビジネスからITへ、教育からIT・ビジネスへと三位一体でスピーディに社会の発展に貢献しつづけます。
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="absolute bottom-[120px] right-[120px] h-[120px] w-[120px]">
-      <img
-        src="/src/assets/images/index_main/25th.png"
-        alt=""
-      />
     </div>
   </div>
 </template>
---
+++ src/pages/si-service-business/components/Automated-Testing-Tools.vue
@@ -0,0 +1,115 @@
+<template>
+  <div
+    id="AutomatedTestingTools"
+    data-aos="fade-in"
+    class="w-full"
+    :style="{ width: MAIN_CONTENT_WIDTH + 'px' }"
+  >
+    <div
+      class="max-w-[600px] flex justify-center bg-[#6785C1] px-[44px] py-[17px] text-[36px] font-[500] text-white"
+    >
+      携帯アプリテスト自動化ツール
+    </div>
+    <div
+      v-for="(item, index) in ExperienceData"
+      :key="index"
+      class="mt-[60px] min-h-[360px] w-full flex justify-between"
+    >
+      <div
+        :class="index % 2 != 0 ? 'order-1' : ''"
+        class="w-[calc(50%-15px)] flex flex-col justify-between"
+      >
+        <div class="font-NotoSansJP text-[36px] font-[700] text-[#071027]">
+          {{ item.title }}
+        </div>
+        <ul class="w-full">
+          <li
+            v-for="(items, i) in item.content"
+            :key="i"
+            class="mt-[30px] w-full flex px-[30px]"
+          >
+            <div class="mt-[5px] h-full min-w-[16px]">
+              <img
+                class="h-16px w-[16px]"
+                src="@/assets/images/company_profile/rhombic.svg"
+                alt=""
+              />
+            </div>
+            <p class="ml-[10px]">{{ items.list }}</p>
+          </li>
+        </ul>
+      </div>
+      <div class="w-[calc(50%-15px)]">
+        <div class="h-full w-full">
+          <img
+            class="h-full w-full"
+            :src="item.Image"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { MAIN_CONTENT_WIDTH } from '@/config/UI'
+import { ExperienceType } from '../types/index'
+import SI7 from '@/assets/images/si-service-business/si_7.jpg'
+import SI5 from '@/assets/images/si-service-business/si_5.png'
+import SI6 from '@/assets/images/si-service-business/si_6.png'
+
+const ExperienceData = ref<ExperienceType[]>([
+  {
+    title: '携帯アプリテストの課題',
+    Image: SI7,
+    content: [
+      { list: 'システム設計～開発～保守・運用' },
+      {
+        list: '時代の進歩に伴い、モバイルテクノロジーは日々変化しております。'
+      },
+      {
+        list: '携帯電話端末のメーカーや携帯モデルの数が多く、オペレーティングシステムの更新も早く、モバイルアプリケーションのアップグレードも頻繁に行われるため、携帯アプリを開発する企業にとって、テストのコストと効率の面でボトルネックとなっています。 テストリソースを有効的に活用し、多次元の検査やテストを実行し、手動の作業負荷を軽減し、継続的に開発品質をモニタリングする機能が緊急のニーズとなっています。'
+      }
+    ]
+  },
+  {
+    title: '携帯テスト自動化ツール',
+    Image: SI5,
+    content: [
+      {
+        list: '弊社が提供する自動テストプラットフォームはBDDコンセプト（行動駆動型開発）に基づいて設計・開発されております。大企業や中堅企業のテスト人件費を効果的に削減し、ビジネスイノベーションを促進することができました。銀行、証券、製造業などの企業で長年にわたり運用されています。以下の特徴を持っています。'
+      },
+      {
+        list: 'Webブラウザ：　Edge、IE、Google、Firefox、Safari対応'
+      },
+      { list: 'スピードが素早い、無人化' },
+      { list: '自然言語で自動化スクリプトを作成' },
+      { list: 'リモートで実機を操作' },
+      { list: '稼働監視とリアルタイム監視' }
+    ]
+  },
+  {
+    title: '本自動テストプラットフォームを利用すると',
+    Image: SI6,
+    content: [
+      {
+        list: '繰り返すテスト作業が機械に置き換えられます'
+      },
+      {
+        list: '手動ではカバーできないテスト範囲'
+      },
+      { list: 'テストレポートを自動生成' },
+      { list: '多数のテストケースを迅速に実行できる' },
+      { list: '人間の介入なしでバッチで実行可能' },
+      { list: '製品に対して、厳格かつ全面的なテストを実施' },
+      {
+        list: 'テスターはケース設計、プロセス設計、およびより深いテスト作業に重点を置きます。'
+      },
+      { list: '頻繁なコード改修による多数の回帰テスト' },
+      { list: '手動テストのリスクが大幅に減少' }
+    ]
+  }
+])
+</script>
+<style lang=""></style>
