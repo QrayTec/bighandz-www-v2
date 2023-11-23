4e3cac0abdef9abe281d323113178ee7711dd88c
shouzia <saopipizhu@163.com>
2023-11-24 00:46:37 +0800

    🐛修复隐私链接 文案

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -53,8 +53,8 @@ interface FooterNavData {
 
 const NavData = ref<FooterNavData[]>([
   { id: 1, title: 'ホーム', href: '/' },
-  { id: 1, title: 'プライバシーポリシー', href: '/CustomSize–2.pdf' },
-  { id: 1, title: '個人情報の取扱いについて', href: '/CustomSize–1.pdf' }
+  { id: 1, title: '利用規約', href: '/CustomSize–2.pdf' },
+  { id: 1, title: '個人情報保護方針', href: '/CustomSize–1.pdf' }
 ])
 </script>
 
