ec99ccddb40639f0554c1b0809a3d2154b7da75e
shouzia <saopipizhu@163.com>
2023-12-01 23:50:28 +0800

    🐛修复邮箱过短无法验证

--- src/pages/contact-us/components/Content.vue
+++ src/pages/contact-us/components/Content.vue
@@ -190,8 +190,8 @@ const generateMailtoLink = () => {
   }
 
   // 添加对电子邮件格式的验证
-  const emailRegex =
-    /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
   if (!emailRegex.test(Email.value)) {
     alert('メールアドレスの形式が正しくありません')
     return
