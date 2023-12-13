143bf30278760121983e6737488db6af22cb297d
shouzia <saopipizhu@163.com>
2023-12-14 00:44:46 +0800

    ✨添加事業内容・グループ企業公司信息

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -28,6 +28,7 @@
       <div class="index_main">
         <First />
         <Business />
+        <BusinessCompanies />
         <!-- <Businesscontent /> -->
         <!-- <BusinessEcharts /> -->
         <!-- <Field /> -->
@@ -44,6 +45,7 @@ import First from './components/First.vue'
 import Business from './components/Business.vue'
 // import Businesscontent from './components/Business-content.vue'
 // import BusinessEcharts from './components/BusinessEcharts.vue'
+import BusinessCompanies from './components/Business-Companies.vue'
 import videoFile from '@/assets/video/main_video.mp4'
 
 const video = ref<HTMLVideoElement>()
