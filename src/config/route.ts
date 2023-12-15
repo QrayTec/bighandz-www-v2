3da0cdc826a667ad818de92f8e263603f8d7dbb1
shouzia <saopipizhu@163.com>
2023-12-15 23:32:56 +0800

    🐛修改路由动画时间

--- src/config/route.ts
+++ src/config/route.ts
@@ -4,14 +4,14 @@ import { onBeforeRouteLeave } from 'vue-router'
 
 export const useScrollToTop = () => {
   const scrollToTop = () => {
-    const scrollStep = -window.scrollY / 300
+    const scrollStep = -window.scrollY / 10
     const scrollInterval = setInterval(() => {
       if (window.scrollY !== 0) {
         window.scrollBy(0, scrollStep)
       } else {
         clearInterval(scrollInterval)
       }
-    }, 15)
+    }, 1)
   }
 
   onBeforeRouteLeave(() => {
