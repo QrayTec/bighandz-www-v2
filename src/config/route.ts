e5bda750a8466fc24b1ee90568533931b375bf0b
shouzia <saopipizhu@163.com>
2023-12-15 23:27:14 +0800

    🐛修复路由过去会立马显示首页

--- src/config/route.ts
+++ src/config/route.ts
@@ -3,7 +3,18 @@
 import { onBeforeRouteLeave } from 'vue-router'
 
 export const useScrollToTop = () => {
+  const scrollToTop = () => {
+    const scrollStep = -window.scrollY / 300
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep)
+      } else {
+        clearInterval(scrollInterval)
+      }
+    }, 15)
+  }
+
   onBeforeRouteLeave(() => {
-    window.scrollTo(0, 0)
+    scrollToTop()
   })
 }
