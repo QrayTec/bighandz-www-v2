2dec57541a1051a20aeac8b920be56fbe431a16f
shouzia <saopipizhu@163.com>
2024-01-09 10:48:44 +0800

    🤔修改视频源

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,7 +2,7 @@
   <div class="index-page relative">
     <div class="index-bg">
       <div class="h-screen w-screen">
-        <!-- <video
+        <video
           v-if="MAIN_CONTENT_WIDTH == 1200"
           ref="video"
           class="h-screen w-screen object-cover"
@@ -11,40 +11,23 @@
           muted
           playsinline
         >
-          <source src="/main_video.mp4" />
-        </video> -->
-        <div
+          <source src="https://myweb-aboutme.vercel.app/main_video.mp4" />
+          <!-- <source src="/main_video.mp4" /> -->
+        </video>
+        <!-- <div
           v-if="MAIN_CONTENT_WIDTH == 1200"
           class="video-container"
         >
           <div class="video-foreground">
             <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/7yTtd981dZI?&autoplay=1&mute=1&controls=0&playsinline=0&version=3&loop=1&playlist=7yTtd981dZI&rel=0"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen
-            ></iframe>
-            <!-- <iframe
               class="h-screen w-screen object-cover"
-              src="https://www.youtube.com/embed/7yTtd981dZI?&autoplay=1&mute=1&controls=0&playsinline=0&version=3&loop=1&playlist=_Sl8diqCAFw&rel=0"
+              src="https://www.youtube.com/embed/7yTtd981dZI?autoplay=1&mute=1&controls=0&playsinline=0&version=3&loop=1&playlist=7yTtd981dZI&rel=0"
               frameborder="0"
               allowfullscreen
               allow="autoplay"
-            ></iframe> -->
-            <!-- <iframe
-              src="//player.bilibili.com/player.html?aid=213378163&bvid=BV1ma411v7Hm&cid=582587711&p=1"
-              scrolling="no"
-              border="0"
-              frameborder="no"
-              framespacing="0"
-              allowfullscreen="true"
-            >
-            </iframe> -->
+            ></iframe>
           </div>
-        </div>
+        </div> -->
 
         <img
           v-else
